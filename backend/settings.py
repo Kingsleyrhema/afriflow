a3c1846df10532f612a91bf5a26478ae26c13342
Kingsleyrhema <kingsleyrhema135@gmail.com>
2025-04-23 23:55:59 +0100

    2nd commit

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!==cr-bhat1)4cjb+!j^@+qupg9_#ur=(lnjj6l*_*oau%-vhb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
