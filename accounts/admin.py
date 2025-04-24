0213885c61aa87d8b789b1055e6fb1398876e06c
Kingsleyrhema <kingsleyrhema135@gmail.com>
2025-04-24 01:41:06 +0100

    update admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+import models
 
+admin.site.register(models.CustomUser)
+admin.site.register(models.CustomUserManager)  # Register the CustomUser model with the admin site
 # Register your models here.
