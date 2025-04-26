4cd620ea7d00e4260946f5f9b3ed20e4ff6eb7bf
Kingsleyrhema <kingsleyrhema135@gmail.com>
2025-04-26 22:46:54 +0100

    updated static files

--- backend/settings.py
+++ backend/settings.py
@@ -129,9 +129,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+import os
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+BASE_DIR / "accounts/static",
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
