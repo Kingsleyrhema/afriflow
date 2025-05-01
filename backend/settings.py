e79725737e91dee5f00160968e706d103c87c261
Kingsleyrhema <kingsleyrhema135@gmail.com>
2025-05-01 12:49:48 +0100

    updating chatbot

--- backend/settings.py
+++ backend/settings.py
@@ -90,6 +90,7 @@ DATABASES = {
         'HOST': 'paybridge.mysql.pythonanywhere-services.com',
         'PORT': '3306',
         'OPTIONS': {
+            'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
